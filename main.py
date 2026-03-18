a3c54a8a91906624cd5fa818734c6904612fefdd
Triananda Fajar <mail.trianandafajar@gmail.com>
2026-03-18 10:06:34 +0700

    fix: minor bug

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def root():
 app.add_middleware(RequestIDMiddleware)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
